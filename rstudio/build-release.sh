f84a39f72486bd93782211b7a7cc1554bec40cb7
Mustafa Arif <mustafa@emarif.com>
2024-05-15 09:20:15 +0100

    Update build-release.sh

--- rstudio/build-release.sh
+++ rstudio/build-release.sh
@@ -4,7 +4,8 @@
 
 R_VERSION=$1
 R_MAJOR=$2
-RSTUDIO_VERSION=2024.04.0-735-amd64
+#RSTUDIO_VERSION=2024.04.0-735-amd64
+RSTUDIO_VERSION=2023.12.1-402-amd64
 
 mkdir -p logs buildfiles output
 
@@ -12,6 +13,6 @@ cp ./template/rstudio-build.def ./buildfiles/rstudio-build-$R_VERSION.def
 
 sed -i "s/R_VERSION=.*/R_VERSION=$R_VERSION/" ./buildfiles/rstudio-build-$R_VERSION.def
 sed -i "s/R_MAJOR=.*/R_MAJOR=$R_MAJOR/" ./buildfiles/rstudio-build-$R_VERSION.def
-sed -i "s/RSTUDIO_VERSION=.*/RSTUDIO_VERSION=$R_MAJOR/" ./buildfiles/rstudio-build-$R_VERSION.def
+sed -i "s/RSTUDIO_VERSION=.*/RSTUDIO_VERSION=$RSTUDIO_VERSION/" ./buildfiles/rstudio-build-$R_VERSION.def
 
-singularity build ./output/rstudio-$R_VERSION.sif ./buildfiles/rstudio-build-$R_VERSION.def | tee ./logs/rstudio-$R_VERSION-$(date +"%d-%m-%Y")-build.log
+singularity build ./output/rstudio_${R_VERSION}_${RSTUDIO_VERSION}.sif ./buildfiles/rstudio-build-$R_VERSION.def | tee ./logs/rstudio-$R_VERSION-$(date +"%d-%m-%Y")-build.log
